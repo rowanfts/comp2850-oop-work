98dc9895e1c250da1a1508deac7f7e5994febf7c
Rowan Fartousi <146141872+rowanfts@users.noreply.github.com>
2025-10-03 23:59:13 +0000

    TypeTasks

---
+++ tasks/task2_5_1/Task.kt
@@ -0,0 +1,6 @@
+fun main () {
+	var num = 42
+	println(num)
+    num = 69
+    println(num)
+}
--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,10 @@
 // Task 3.1: command line arguments
+fun main(args: Array<String>) {
+    if (args.size == 2) {
+    	println(args[0])
+    	println(args[1])
+    }
+    else {
+        exitProcess(1)
+    }
+} 
