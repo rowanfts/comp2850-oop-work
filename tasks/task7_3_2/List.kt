49dec2100157cdbe5e1b38a1a1c2fc68eee7e61c
Rowan Fartousi <146141872+rowanfts@users.noreply.github.com>
2025-10-14 12:39:21 +0000

    Collection Types1

--- tasks/task7_3_2/List.kt
+++ tasks/task7_3_2/List.kt
@@ -1,6 +1,6 @@
 // Task 7.3.1: list element access
 fun main() {
-    val numbers = listOf(9, 3, 6, 2, 8, 5)
+    val numbers = mutableListOf(9, 3, 6, 2, 8, 5)
     println(numbers)   
     println(numbers[0])
     println(numbers.get(0))    
@@ -8,6 +8,9 @@ fun main() {
     println(numbers.slice(2..4))
     println(numbers.first())
     println(numbers.last())
-    //numbers[0]=3
-    //numbers.add(3)
+    numbers[0]=3
+    numbers.add(3)
+    println(numbers)  
+    numbers.remove(3)
+    println(numbers)   
 }
--- tasks/task7_7_1/Stats.kt
+++ tasks/task7_7_1/Stats.kt
@@ -1,1 +1,23 @@
 // Task 7.7.1: stats for a numeric dataset
+fun main() {
+    medianComp(readData())
+}
+fun readData() = buildList {
+    while (true) {
+        print("enter a number, or press ENTER when done: ")
+        var input = readln()
+        if (input.isEmpty()) {
+            break
+        }
+        add(input.toFloat())
+    }
+}
+fun medianComp(nums: List<Float>) {
+    println(nums.sorted())
+    if (nums.size %2 == 0) {
+        println( (nums.sorted().get(nums.size/2) + nums.sorted().get((nums.size/2)-1))/2 )
+    }
+    else {
+        println( nums.sorted().get(nums.size/2) )
+    }    
+}
