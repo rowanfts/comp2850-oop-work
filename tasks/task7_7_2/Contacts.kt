67063dba855305eab1e2fbd2ea99a363a2df824d
Rowan Fartousi <146141872+rowanfts@users.noreply.github.com>
2025-10-14 22:56:31 +0000

    Collection Types3

--- tasks/task7_7_2/Contacts.kt
+++ tasks/task7_7_2/Contacts.kt
@@ -1,1 +1,20 @@
 // Task 7.7.2: contact database, using a map
+fun main () {
+    val contacts = mutableMapOf (
+        "John" to "1234567890",     //storing phone numbers as Int
+        "Sarah" to "1111111111"     //didn't work. Should I use Long?
+    ) 
+
+    print("Enter a name: ")
+    val name = readln()
+    if (name in contacts.keys) {
+        println("Contact found.")
+        println(contacts[name])
+    }
+    else {
+        print("Contact not found. \nPlease insert ${name}'s phone number: ")
+        val number = readln()
+        contacts[name] = number
+        println("The following information has been added to your contacts:\n${name}, $number")
+    }    
+}
