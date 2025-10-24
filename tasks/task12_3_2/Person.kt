1f6b85317d4ca4e6f07a8b1bc65e749aed46109a
Rowan Fartousi <146141872+rowanfts@users.noreply.github.com>
2025-10-24 12:20:41 +0000

    Classes 2

--- tasks/task12_3_2/Person.kt
+++ tasks/task12_3_2/Person.kt
@@ -1,1 +1,13 @@
 // Task 12.3.2: Person class, with a secondary constructor
+import java.time.LocalDate
+
+class Person(var name: String, val birth: LocalDate) {
+    var isMarried = false
+    constructor(birth: String): this(LocalDate.parse(birth)) // Somnething's wrong
+}
+fun main () {
+    val date = LocalDate.of(2005, 7, 1)
+    val p = Person("Rowan", "2005-07-01")
+    println(p.name)
+    println(p.birth)
+}
