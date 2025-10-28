91561f9e99210aac13d9adfb30d72dd07672d69e
Rowan Fartousi <146141872+rowanfts@users.noreply.github.com>
2025-10-28 11:50:17 +0000

    Classes 3 (task 12.3)

--- tasks/task12_3_3/Person.kt
+++ tasks/task12_3_3/Person.kt
@@ -3,13 +3,19 @@ import java.time.LocalDate
 
 class Person(var name: String, val birth: LocalDate) {
     constructor(name: String, birth: String): this(name, LocalDate.parse(birth)) 
-    var isMarried = falseinit {
+    var isMarried = false
+    init {
         require(name.isNotBlank()) { "Name cannot be blank" }
     }
 }
 fun main () {
-    val date = LocalDate.of(2005, 7, 1)
-    val p = Person("Rowan", "2005-07-01")
+    print("Enter your name: ")
+    val userName = readln()
+    print("\nEnter your date of birth (YYYY-MM-DD): ")
+    val userDate = readln()
+
+    val p = Person(userName, userDate)
+
     println(p.name)
     println(p.birth)
 }
